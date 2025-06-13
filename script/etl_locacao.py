871a5c12dbbbde6cfa8aeb9102980f8a1baf856b
Gabriel Vieira <gabriel12.leo@gmail.com>
2025-06-13 00:48:22 -0300

    Revert "new dbs, new data, updated scripts:boom::monocle_face:"
    This reverts commit ec7f684c2be917d75e19579dcb9e183e972eb64e.

--- script/etl_locacao.py
+++ script/etl_locacao.py
@@ -1,42 +1,14 @@
-# import pandas as pd
-# import sqlite3
-
-# # df = pd.read_csv('data/dados_locacao_recife_tratado.csv', sep=';')
-# # df = pd.read_csv('data/dados_locacao_fortaleza_tratado.csv', sep=';')
-# # df = pd.read_csv('data/dados_locacao_natal_tratado.csv', sep=';')
-# # df = pd.read_csv('data/dados_locacao_maceio_tratado.csv', sep=';')
-# df = pd.read_csv('data/dados_locacao_aracaju_tratado.csv', sep=';')
-
-# print(df.head())      
-# print(df.columns)      
-
-
-# conn = sqlite3.connect('db/dados_locacao_aracaju.db')
-
-# df.to_sql('tabela_dados_locacao', conn, index=False)
-
-# conn.close()
-
 import pandas as pd
 import sqlite3
 
-cidades = [
-    'recife',
-    'fortaleza'
-    'maceio',
-    'aracaju',
-    'salvador',
-    'sao_luis',
-    'teresina',
-    'joao_pessoa'
-]
+df = pd.read_csv('data/dados_locacao_tratado.csv', sep=';')
 
-for cidade in cidades:
-    nome_csv = f'data/dados_locacao_{cidade}_tratado.csv'
-    nome_db = f'db/dados_locacao_{cidade}.db'
+print(df.head())      
+print(df.columns)      
 
-    df = pd.read_csv(nome_csv, sep=';')
- 
-    conn = sqlite3.connect(nome_db)
-    df.to_sql('tabela_dados_locacao', conn, if_exists='replace', index=False)
-    conn.close()
+
+conn = sqlite3.connect('db/dados_locacao.db')
+
+df.to_sql('tabela_dados_locacao', conn, index=False)
+
+conn.close()
--- script/etl_vendas.py
+++ script/etl_vendas.py
@@ -1,26 +1,16 @@
 import pandas as pd
 import sqlite3
 
-cidades = [
-    'recife',
-    'fortaleza'
-    'maceio',
-    'aracaju',
-    'salvador',
-    'sao_luis',
-    'teresina',
-    'joao_pessoa'
-]
 
-for cidade in cidades:
-    nome_csv = f'data/dados_vendas_{cidade}_tratados.csv'
-    nome_db = f'db/dados_vendas_{cidade}.db'
+df = pd.read_csv('data/dados_vendas_tratados.csv', sep=';')
 
-    df = pd.read_csv(nome_csv, sep=';')
- 
-    conn = sqlite3.connect(nome_db)
-    df.to_sql('tabela_dados_vendas', conn, if_exists='replace', index=False)
-    conn.close()
+print(df.head())      
+print(df.columns)      
 
 
+conn = sqlite3.connect('db/dados_vendas.db')
 
+
+df.to_sql('tabela_dados_vendas', conn, if_exists='replace', index=False)
+
+conn.close()
