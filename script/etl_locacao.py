ec7f684c2be917d75e19579dcb9e183e972eb64e
Gabriel Vieira <gabriel12.leo@gmail.com>
2025-06-12 21:40:07 -0300

    new dbs, new data, updated scripts:boom::monocle_face:

--- script/etl_locacao.py
+++ script/etl_locacao.py
@@ -1,14 +1,42 @@
+# import pandas as pd
+# import sqlite3
+
+# # df = pd.read_csv('data/dados_locacao_recife_tratado.csv', sep=';')
+# # df = pd.read_csv('data/dados_locacao_fortaleza_tratado.csv', sep=';')
+# # df = pd.read_csv('data/dados_locacao_natal_tratado.csv', sep=';')
+# # df = pd.read_csv('data/dados_locacao_maceio_tratado.csv', sep=';')
+# df = pd.read_csv('data/dados_locacao_aracaju_tratado.csv', sep=';')
+
+# print(df.head())      
+# print(df.columns)      
+
+
+# conn = sqlite3.connect('db/dados_locacao_aracaju.db')
+
+# df.to_sql('tabela_dados_locacao', conn, index=False)
+
+# conn.close()
+
 import pandas as pd
 import sqlite3
 
-df = pd.read_csv('data/dados_locacao_tratado.csv', sep=';')
+cidades = [
+    'recife',
+    'fortaleza'
+    'maceio',
+    'aracaju',
+    'salvador',
+    'sao_luis',
+    'teresina',
+    'joao_pessoa'
+]
 
-print(df.head())      
-print(df.columns)      
+for cidade in cidades:
+    nome_csv = f'data/dados_locacao_{cidade}_tratado.csv'
+    nome_db = f'db/dados_locacao_{cidade}.db'
 
-
-conn = sqlite3.connect('db/dados_locacao.db')
-
-df.to_sql('tabela_dados_locacao', conn, index=False)
-
-conn.close()
+    df = pd.read_csv(nome_csv, sep=';')
+ 
+    conn = sqlite3.connect(nome_db)
+    df.to_sql('tabela_dados_locacao', conn, if_exists='replace', index=False)
+    conn.close()
